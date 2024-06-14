96d839aabf2f16460679b5810522e4bd8dc873c0
eunhye <dmsp2377@naver.com>
2024-06-14 11:00:07 +0900

    cors modify = delete /

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
     credentials: true,
   },
